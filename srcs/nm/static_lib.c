e28f8de5eabfd87f8c0398645d6a540b2df53dbb
jcamhi <jeremshy@h3lp.fr>
2017-07-21 16:51:15 +0200

    static_lib.c

--- srcs/nm/static_lib.c
+++ srcs/nm/static_lib.c
@@ -1,13 +1,22 @@
 #include <nm.h>
 
-static void	handle_obj_sl(t_data *data, uint32_t offset, uint32_t max)
+static void		fin_obj_sl(t_data *data)
 {
-	struct ar_hdr *hdr;
-	void	*obj;
+	print_list(data, data->list);
+	delete_list(data->list);
+	data->list = NULL;
+	if (data->sections)
+		free(data->sections);
+	data->sections = NULL;
+}
+
+static void		handle_obj_sl(t_data *data, uint32_t offset, uint32_t max)
+{
+	struct ar_hdr	*hdr;
+	void			*obj;
 
 	hdr = data->binary + offset;
 	obj = (void*)(hdr + 1);
-	// ft_printf("max : %#X\n", max);
 	while (offset <= max)
 	{
 		ft_printf("\n%s(%s):\n", data->av, hdr + 1);
@@ -18,25 +27,18 @@ static void	handle_obj_sl(t_data *data, uint32_t offset, uint32_t max)
 		{
 			obj = (void*)obj + 1;
 			data->magic = *(uint32_t*)(obj);
-			// if ((uint32_t)(obj - data->binary) > max)
-			// 	return ;
 		}
 		if (data->magic == MH_MAGIC_64)
 			handle_64(data, obj - data->binary, 0);
 		else if (data->magic == MH_MAGIC)
 			handle_32(data, obj - data->binary, 0);
 		offset += ft_atoi(hdr->ar_size) + sizeof(struct ar_hdr);
-		print_list(data, data->list);
-		delete_list(data->list);
-		data->list = NULL;
-		if (data->sections)
-			free(data->sections);
-		data->sections = NULL;
+		fin_obj_sl(data);
 		hdr = data->binary + offset;
 	}
 }
 
-uint32_t	find_first_obj(t_data *data, uint32_t offset)
+uint32_t		find_first_obj(t_data *data, uint32_t offset)
 {
 	void	*obj;
 
@@ -48,26 +50,32 @@ uint32_t	find_first_obj(t_data *data, uint32_t offset)
 	return (obj + 4 - data->binary - sizeof(struct ar_hdr));
 }
 
-void	handle_static_lib(t_data *data, uint32_t offset)
+static int		handle_weird_lib(t_data *data, struct ranlib *symtab)
+{
+	uint32_t		min;
+
+	min = find_first_obj(data, (void*)symtab - data->binary);
+	handle_obj_sl(data, min, data->end - 1);
+	return (1);
+}
+
+void			handle_static_lib(t_data *data, uint32_t offset)
 {
 	struct ranlib	*symtab;
 	struct ranlib	*start;
-	uint32_t			*nbr;
-	uint32_t			min;
-	uint32_t			max;
+	uint32_t		*nbr;
+	uint32_t		min;
+	uint32_t		max;
 
-	nbr = (uint32_t*)(data->binary + offset + SARMAG + sizeof(struct ar_hdr) + sizeof(char[20]));
-	symtab = data->binary + offset + SARMAG + sizeof(struct ar_hdr) + sizeof(char[20]) + sizeof(uint32_t);
-
+	nbr = (uint32_t*)(data->binary + offset + SARMAG + sizeof(struct ar_hdr)
+		+ sizeof(char[20]));
+	symtab = data->binary + offset + SARMAG + sizeof(struct ar_hdr)
+		+ sizeof(char[20]) + sizeof(uint32_t);
 	start = symtab;
 	min = 0;
 	max = 0;
-	if (*nbr == 0)
-	{
-		min = find_first_obj(data, (void*)symtab - data->binary);
-		handle_obj_sl(data, min, data->end - 1);
+	if (*nbr == 0 && handle_weird_lib(data, symtab))
 		return ;
-	}
 	while ((void*)symtab < (void*)start + *nbr)
 	{
 		if (min == 0 || symtab->ran_off < min)
