357bd348f5bb8b2a4843facce3b38e786432ee9b
jcamhi <jeremshy@h3lp.fr>
2017-07-21 16:59:16 +0200

    Rainbow Dash is best pony !

--- includes/nm.h
+++ includes/nm.h
@@ -1,12 +1,12 @@
 #ifndef NM_H
 # define NM_H
 
-#ifdef __x86_64__
-	# define CPUTYPE CPU_TYPE_X86_64
-#endif
-#ifdef __i386__
-	# define CPUTYPE CPU_TYPE_I386
-#endif
+# ifdef __x86_64__
+#  define CPUTYPE CPU_TYPE_X86_64
+# endif
+# ifdef __i386__
+#  define CPUTYPE CPU_TYPE_I386
+# endif
 
 # include <stdio.h>
 # include <ft_printf.h>
@@ -20,11 +20,11 @@ typedef struct	s_symbole
 	void				*addr;
 	char				*str;
 	uint64_t			value;
-	char			sym;
-	int64_t		n_strx;
-	int				is_debug;
-	uint32_t	magic;
-	size_t		poids;
+	char				sym;
+	int64_t				n_strx;
+	int					is_debug;
+	uint32_t			magic;
+	size_t				poids;
 	struct s_symbole	*next;
 }				t_symbole;
 
@@ -37,24 +37,26 @@ typedef struct	s_data
 	uint32_t	strsize;
 	uint32_t	symoff;
 	uint32_t	nsyms;
-	char			*sections;
+	char		*sections;
 	uint32_t	nbsect;
 	uint32_t	end;
-	const char			*av;
+	const char	*av;
 }				t_data;
 
-void				find_boundaries_64(t_data *data, uint64_t offset);
-void				find_boundaries_32(t_data *data, uint64_t offset);
-void				handle_32(t_data *data, uint64_t offset, size_t poids);
-void				handle_64(t_data *data, uint64_t offset, size_t poids);
-void				create_list_64(t_data *data, size_t poids);
-void				create_list_32(t_data *data, size_t poids);
+void			find_boundaries_64(t_data *data, uint64_t offset);
+void			find_boundaries_32(t_data *data, uint64_t offset);
+void			handle_32(t_data *data, uint64_t offset, size_t poids);
+void			handle_64(t_data *data, uint64_t offset, size_t poids);
+void			create_list_64(t_data *data, size_t poids);
+void			create_list_32(t_data *data, size_t poids);
 t_symbole		*create_elem(t_data *data, uint64_t offset, size_t poids);
-void				add_elem_start(t_data *data, t_symbole **list, uint64_t offset, size_t poids);
-t_symbole		*add_elem_end(t_data *data, t_symbole *list, uint64_t offset, size_t poids);
-void				print_list(t_data *data, t_symbole *list);
-void				delete_list(t_symbole *list);
+void			add_elem_start(t_data *data, t_symbole **list, uint64_t offset,
+	size_t poids);
+t_symbole		*add_elem_end(t_data *data, t_symbole *list, uint64_t offset,
+	size_t poids);
+void			print_list(t_data *data, t_symbole *list);
+void			delete_list(t_symbole *list);
 t_symbole		*ft_sort(t_symbole *list);
-void				handle_fat_cigam(t_data *data);
-void				handle_static_lib(t_data *data, uint32_t offset);
+void			handle_fat_cigam(t_data *data);
+void			handle_static_lib(t_data *data, uint32_t offset);
 #endif
