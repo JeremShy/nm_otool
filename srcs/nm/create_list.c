e71ffdb7c2c76163dbd10bd3b3ad66d947d13bbf
jcamhi <jeremshy@h3lp.fr>
2017-07-21 16:36:12 +0200

    create_list.c

--- srcs/nm/create_list.c
+++ srcs/nm/create_list.c
@@ -2,15 +2,16 @@
 
 void	create_list_64(t_data *data, size_t poids)
 {
-	struct nlist_64 *array;
-	uint32_t	i;
+	struct nlist_64	*array;
+	uint32_t		i;
 
 	array = (struct nlist_64*)((void*)(data->binary) + data->symoff);
 	i = 0;
 	data->list = NULL;
-	while(i < data->nsyms)
+	while (i < data->nsyms)
 	{
-		data->list = add_elem_end(data, data->list, (uint64_t)(data->symoff + i * sizeof(struct nlist_64)), poids);
+		data->list = add_elem_end(data, data->list, (uint64_t)(data->symoff +
+					i * sizeof(struct nlist_64)), poids);
 		i++;
 	}
 	data->list = ft_sort(data->list);
@@ -18,15 +19,16 @@ void	create_list_64(t_data *data, size_t poids)
 
 void	create_list_32(t_data *data, size_t poids)
 {
-	struct nlist *array;
-	uint32_t	i;
+	struct nlist	*array;
+	uint32_t		i;
 
 	array = (struct nlist*)((void*)(data->binary) + data->symoff);
 	i = 0;
 	data->list = NULL;
-	while(i < data->nsyms)
+	while (i < data->nsyms)
 	{
-		data->list = add_elem_end(data, data->list, (uint64_t)(data->symoff + i * sizeof(struct nlist)), poids);
+		data->list = add_elem_end(data, data->list, (uint64_t)(data->symoff +
+					i * sizeof(struct nlist)), poids);
 		i++;
 	}
 	data->list = ft_sort(data->list);
