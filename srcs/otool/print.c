d0ec8348be4b7cf2f6ea31d08b22cd969324a27f
jcamhi <jeremshy@h3lp.fr>
2017-07-25 18:31:03 +0200

    print.c

--- srcs/otool/print.c
+++ srcs/otool/print.c
@@ -6,13 +6,23 @@
 /*   By: jcamhi <jcamhi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/24 15:36:46 by jcamhi            #+#    #+#             */
-/*   Updated: 2017/07/25 17:53:16 by jcamhi           ###   ########.fr       */
+/*   Updated: 2017/07/25 18:30:57 by jcamhi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <otool.h>
 
-void	print(t_data *data, void *start, uint64_t size, uint64_t print_size)
+static void	print_addr(t_data *data, uint64_t print_size, uint64_t offset)
+{
+	if (offset > (uint64_t)data->size)
+		return (set_error_and_return(data));
+	if (data->magic == MH_MAGIC || data->magic == MH_CIGAM)
+		ft_printf("%08llx\t", print_size + offset);
+	else if (data->magic == MH_MAGIC_64)
+		ft_printf("%016llx\t", print_size + offset);
+}
+
+void		print(t_data *data, void *start, uint64_t size, uint64_t print_size)
 {
 	uint64_t	offset;
 	int			i;
@@ -22,22 +32,17 @@ void	print(t_data *data, void *start, uint64_t size, uint64_t print_size)
 	i = 0;
 	while (offset < size)
 	{
-		if (offset > (uint64_t)data->size)
-			return (set_error_and_return(data));
-		if (data->magic == MH_MAGIC || data->magic == MH_CIGAM)
-			ft_printf("%08llx\t", print_size + offset);
-		else if (data->magic == MH_MAGIC_64)
-			ft_printf("%016llx\t", print_size + offset);
+		print_addr(data, print_size, offset);
 		i = 0;
-		while (i < 0x10 && offset + i < size && offset + i < (uint64_t)data->size)
+		while (i < 0x10 && offset + i < size && offset + i
+			< (uint64_t)data->size)
 		{
 			if (data->magic == MH_CIGAM)
 				ft_printf("%02x", *((unsigned char*)(start + offset + i)));
 			else
 				ft_printf("%02x ", *((unsigned char*)(start + offset + i)));
 			i++;
-			if (data->magic == MH_CIGAM && i % 4 == 0)
-				ft_printf(" ");
+			(data->magic == MH_CIGAM && i % 4 == 0) ? ft_printf(" ") : 0;
 		}
 		if (offset > (uint64_t)data->size)
 			return (set_error_and_return(data));
