7adab04b4b61ebb07b100f1d883091c05e748816
jcamhi <jeremshy@h3lp.fr>
2017-07-25 14:36:30 +0200

    find_boundaries64

--- srcs/nm/find_boundaries_64.c
+++ srcs/nm/find_boundaries_64.c
@@ -6,11 +6,27 @@
 /*   By: jcamhi <jcamhi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/24 15:35:56 by jcamhi            #+#    #+#             */
-/*   Updated: 2017/07/25 13:21:50 by jcamhi           ###   ########.fr       */
+/*   Updated: 2017/07/25 14:36:22 by jcamhi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <nm.h>
+
+static int	get_good_sign_64(t_data *data, struct section_64 *tab,
+	uint32_t i, uint32_t j)
+{
+	if ((void*)(tab + j) > data->tend)
+		return (-1);
+	if (ft_strequ(tab[j].sectname, "__text"))
+		data->sections[j + i] = 'T';
+	else if (ft_strequ(tab[j].sectname, "__bss"))
+		data->sections[j + i] = 'B';
+	else if (ft_strequ(tab[j].sectname, "__data"))
+		data->sections[j + i] = 'D';
+	else
+		data->sections[j + i] = 'S';
+	return (1);
+}
 
 static int	handle_seg_header_64(t_data *data, struct load_command *lc,
 		uint32_t i)
@@ -28,40 +44,22 @@ static int	handle_seg_header_64(t_data *data, struct load_command *lc,
 		return (i);
 	while (j < sc->nsects)
 	{
-		if ((void*)(tab + j) > data->tend)
-			return (-1);
 		data->nbsect++;
-		if (ft_strequ(tab[j].sectname, "__text"))
-			data->sections[j + i] = 'T';
-		else if (ft_strequ(tab[j].sectname, "__bss"))
-			data->sections[j + i] = 'B';
-		else if (ft_strequ(tab[j].sectname, "__data"))
-			data->sections[j + i] = 'D';
-		else
-			data->sections[j + i] = 'S';
+		if (get_good_sign_64(data, tab, i, j) == -1)
+			return (-1);
 		j++;
 	}
 	return (i + j);
 }
 
-int		find_boundaries_64(t_data *data, uint64_t offset)
+int			fill_data_sections_64(t_data *data, struct mach_header_64 *header,
+		struct load_command *lc)
 {
-	struct mach_header_64			*header;
-	struct load_command				*lc;
-	uint32_t						i;
-	int32_t						j;
+	uint32_t				i;
+	int32_t					j;
 
-	header = (struct mach_header_64*)(data->binary + offset);
-	lc = (void*)header + sizeof(struct mach_header_64);
-	if ((void*)lc > data->tend)
-		return (0);
 	i = 0;
 	j = 0;
-	data->nbsect = 0;
-	if (!(data->sections = (char*)malloc(header->ncmds * 20 + 1)))
-		return (0);
-	data->sections = ft_memset(data->sections, 'S', header->ncmds * 20);
-	data->sections[header->ncmds] = '\0';
 	while (i < header->ncmds)
 	{
 		if (lc->cmd == LC_SEGMENT_64)
@@ -76,4 +74,21 @@ int		find_boundaries_64(t_data *data, uint64_t offset)
 		i++;
 	}
 	return (1);
+}
+
+int			find_boundaries_64(t_data *data, uint64_t offset)
+{
+	struct mach_header_64			*header;
+	struct load_command				*lc;
+
+	header = (struct mach_header_64*)(data->binary + offset);
+	lc = (void*)header + sizeof(struct mach_header_64);
+	if ((void*)lc > data->tend)
+		return (0);
+	data->nbsect = 0;
+	if (!(data->sections = (char*)malloc(header->ncmds * 20 + 1)))
+		return (0);
+	data->sections = ft_memset(data->sections, 'S', header->ncmds * 20);
+	data->sections[header->ncmds] = '\0';
+	return (fill_data_sections_64(data, header, lc));
 }
